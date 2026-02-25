acb030ed3b03ced90611b437e1796fcd6ea03335
joaaquin-cstll <joaaquin.cstllo@gmail.com>
2026-02-25 14:36:19 +0100

    CambiarColor

--- JavaScript/listaDeTareas/tarea.js
+++ JavaScript/listaDeTareas/tarea.js
@@ -2,22 +2,25 @@ const buttonAgregar = document.getElementById('agregar');
 const buttonBorrar = document.getElementById('borrar');
 const tareas = document.getElementById('tareas');
 const entrada = document.getElementById('entrada');
+const cambiarColor = document.getElementById('cambiarColor');
 
 //Acciona al clicar la funcion agregar
 buttonAgregar.addEventListener('click',agregar);
 buttonBorrar.addEventListener('click',borrar);
+cambiarColor.addEventListener('click', colorRojo);
+
+function colorRojo() {
+    const lista = document.querySelectorAll('p');
+    for (let i=0; i<lista.length; i++){
+        lista[i].style.color="red";
+    }
+}
 
 function agregar() {
     const pTarea = document.createElement('p');
     const node1 = document.createTextNode('- '+entrada.value);
     pTarea.appendChild(node1);
     tareas.appendChild(pTarea);
-
-    const enlace = document.createElement('a');
-    const node2 = document.createTextNode(' | Enlace a....');
-    enlace.appendChild(node2);
-    enlace.setAttribute('href',"https://www.marca.com");
-    pTarea.appendChild(enlace);
 
     const imagen = document.createElement('img');
     tareas.appendChild(imagen);
