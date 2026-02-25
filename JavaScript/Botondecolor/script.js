fa73ae15d2ee7e5cf9f46573fe37deb7786405cc
joaaquin-cstll <joaaquin.cstllo@gmail.com>
2026-02-25 14:38:10 +0100

    cambiarColor2

---
+++ JavaScript/Botondecolor/script.js
@@ -0,0 +1,6 @@
+  function cambiarColor() {
+            const cuadrado = document.getElementById("cuadrado");
+            cuadrado.style.backgroundColor = "black";
+            cuadrado.style.color = "white";
+            document.body.style.backgroundColor = "salmon";
+        }
