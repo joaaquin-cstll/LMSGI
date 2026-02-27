44dd3e4f5f268eaf1adc9ac884466bb8ec28512c
joaaquin-cstll <joaaquin.cstllo@gmail.com>
2026-02-27 11:12:29 +0100

    AdivinarNum2

--- JavaScript/numAleatorio/numAlea.js
+++ JavaScript/numAleatorio/numAlea.js
@@ -4,14 +4,23 @@ const dato = document.getElementById('entrada');
 
 botonAdivinar.addEventListener('click', adivinar);
 
+let intentos = 0;
 function adivinar(){
     const num = Number(dato.value); //Pasar entrada a número
 
+    if (intentos == 5) {
+        document.getElementById('intentos').textContent = "Has llegado al límite de intentos."
+        return
+    }
     if (num == numeroSecreto) {
         document.getElementById('salida').textContent = "Felicidades has acertado el número!!! Era el "+numeroSecreto
     } else if(num < numeroSecreto){
         document.getElementById('salida').textContent = "El número que buscas es más alto."
-    } else {
+        intentos++;
+        document.getElementById('intentos').textContent = "Llevas "+intentos+" intentos."
+    } else if(num > numeroSecreto){
         document.getElementById('salida').textContent = "El número que buscas es más bajo."
+        intentos++;
+        document.getElementById('intentos').textContent = "Llevas "+intentos+" intentos."
     }
 }
